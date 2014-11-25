b993d1817101b4e091aea6d503d5063a39d1a86b
takuti <k.takuti@gmail.com>
2014-11-25 11:18:25 +0900

    write pydoc of selection sort

--- sort/selection.py
+++ sort/selection.py
@@ -1,4 +1,24 @@
+""" SELECTION SORT
+
+[algorithm]
+  1: Select the minimum element from [1,n], and swap it for the first element
+  2: Select the minimum element from [2,n], and swap it for the second element
+     ...
+n-1: Select the minimum element from [n-1,n], and swap it for the (n-1)-th element
+
+[order]
+O(n^2)
+"""
+
 def sort(A, compare):
+  """
+  [arguments]
+  A -- Given array
+  compare -- lambda function for comparison of two values (ascend or descend)
+
+  [return]
+  Sorted array
+  """
   n = len(A)
   for i in range(n-1):
     selected_i = i
