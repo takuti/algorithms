b0f6fb298ffcdd09f4c02930105ece85b2b1092b
takuti <k.takuti@gmail.com>
2014-12-15 08:58:19 +0900

    implement k-means in python (scipy)

---
+++ k-means/k_means.py
@@ -0,0 +1,31 @@
+# coding: utf-8
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.cluster.vq import vq, kmeans, kmeans2, whiten
+
+def read_iris():
+  """ read iris dataset as a list of values
+  """
+  lines = map(lambda l: l.rstrip('\n'), open('../@data/iris.data').readlines())
+  X = []
+  label = []
+  for l in lines:
+    l = l.split(',')
+    X.append(map(float, l[:4]))
+    label.append(l[4])
+  return X, label
+
+def main():
+  X, label = read_iris()
+  whitened = whiten(X) # normalize
+  centroid, alloc = kmeans2(whitened, k=3)
+  c = [[] for i in range(3)] # k = 3
+  for i in range(len(X)):
+    c[alloc[i]].append(label[i])
+  print c[0]
+  print c[1]
+  print c[2]
+
+if __name__ == '__main__':
+  main()
