e0887f096c8d622e9ac29d94e990ba121fcb99cc
takuti <k.takuti@gmail.com>
2014-12-26 08:15:29 +0900

    support separated arguments: X and y

--- linear-regression/linear_regression.py
+++ linear-regression/linear_regression.py
@@ -2,22 +2,23 @@
 
 import numpy as np
 
-def gradient_descend(data, alpha=0.3):
+def gradient_descend(X, y, alpha=0.3):
+  m = len(X)
+
   def f(t, x):
-    return t[0] + t[1] * x
+    return t[0] * x[0] + t[1] * x[1]
 
   def J(t):
     # Squared error func: (f(x)-y)^2
-    s = sum([(f(t, data[i][0]) - data[i][1]) ** 2 for i in range(len(data))])
-    return s / (2.0 * len(data))
+    s = sum([(f(t, X[i]) - y[i]) ** 2 for i in range(m)])
+    return s / (2.0 * m)
 
   eps = 1e-7
-  m = len(data)
-  theta = np.zeros([m, 1])
+  theta = np.zeros([len(X[0]), 1])
   while True:
     # Simultaneously update
-    tmp0 = theta[0] - alpha * sum([(f(theta, data[i][0]) - data[i][1]) for i in range(m)]) / m
-    tmp1 = theta[1] - alpha * sum([(f(theta, data[i][0]) - data[i][1]) * data[i][0] for i in range(m)]) / m
+    tmp0 = theta[0] - alpha * sum([(f(theta, X[i]) - y[i]) for i in range(m)]) / m
+    tmp1 = theta[1] - alpha * sum([(f(theta, X[i]) - y[i]) * X[i][1] for i in range(m)]) / m
     if abs(J(theta) - J([tmp0, tmp1])) < eps: break
     else: theta = [tmp0, tmp1]
   return theta
@@ -29,14 +30,6 @@ def normal_equation(X, y):
 
 
 def main():
-  # [x, y]
-  data = np.array([[-2, 1.4],
-                   [-1, 1.7],
-                   [ 0, 2.7],
-                   [ 1, 3.4],
-                   [ 2, 3.8]])
-  theta = gradient_descend(data)
-  print 'y = %f + %fx' % tuple(theta)
   X = np.array([[1, -2],
                 [1, -1],
                 [1,  0],
@@ -47,8 +40,8 @@ def main():
                 [2.7],
                 [3.4],
                 [3.8]])
-  theta = normal_equation(X, y)
-  print 'y = %f + %fx' % tuple(theta)
+  print '[Gradient Descend] y = %f + %fx' % tuple(gradient_descend(X,y))
+  print '[Normal Equation] y = %f + %fx' % tuple(normal_equation(X,y))
 
 
 if __name__ == '__main__':
