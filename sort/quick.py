a1cbe1992894a8fb066330d11ef0f6d5a78c59b8
takuti <k.takuti@gmail.com>
2015-01-17 17:57:08 +0900

    [add] randomized-quicksort

--- sort/quick.py
+++ sort/quick.py
@@ -1,6 +1,8 @@
 """ QUICKSORT
 """
 
+import random
+
 def partition(A, p, r):
   pivot = A[r]
   i = p - 1
@@ -11,13 +13,20 @@ def partition(A, p, r):
   A[i+1], A[r] = A[r], A[i+1]
   return i + 1
 
-def sort(A, p, r):
+def randomized_partition(A, p, r):
+  i = random.randint(p, r)
+  A[i], A[r] = A[r], A[i]
+  return partition(A, p, r)
+
+def sort(A, p, r, randomize_flg=False):
   """
   >>> sort([43, 2, 15, 81, 49, 4, 56, 11, 51, 97], 0, 9)
   [2, 4, 11, 15, 43, 49, 51, 56, 81, 97]
+  >>> sort([43, 2, 15, 81, 49, 4, 56, 11, 51, 97], 0, 9, True)
+  [2, 4, 11, 15, 43, 49, 51, 56, 81, 97]
   """
   if p < r:
-    q = partition(A, p, r)
+    q = randomized_partition(A, p, r) if randomize_flg else partition(A, p, r)
     sort(A, p, q-1)
     sort(A, q+1, r)
   return A
