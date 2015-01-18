ed5a458375a0787cc6b095028716583210367b9b
takuti <k.takuti@gmail.com>
2015-01-19 00:35:22 +0900

    [implement] longest-common-subsequence (LCS) problem

---
+++ foundations/dynamic-programming/longest_common_subsequence.py
@@ -0,0 +1,47 @@
+# coding: utf-8
+
+""" longest-common-subsequence (LCS) problem
+d: ↖
+u: ↑
+l: ←
+"""
+
+def lcs_length(X, Y):
+  m = len(X)
+  n = len(Y)
+  b = [0] * m
+  for i in range(m):
+    b[i] = [0] * n
+  c = [0] * (m+1)
+  for i in range(m+1):
+    c[i] = [0] * (n+1)
+  for i in range(1, m+1):
+    for j in range(1, n+1):
+      if X[i-1] == Y[j-1]:
+        c[i][j] = c[i-1][j-1] + 1
+        b[i-1][j-1] = 'd'
+      elif c[i-1][j] >= c[i][j-1]:
+        c[i][j] = c[i-1][j]
+        b[i-1][j-1] = 'u'
+      else:
+        c[i][j] = c[i][j-1]
+        b[i-1][j-1] = 'l'
+  return c, b
+
+def print_lcs(b, X, i, j):
+  if i == 0 or j == 0:
+    return
+  if b[i-1][j-1] == 'd':
+    print_lcs(b, X, i-1, j-1)
+    print X[i-1]
+  elif b[i-1][j-1] == 'u':
+    print_lcs(b, X, i-1, j)
+  else:
+    print_lcs(b, X, i, j-1)
+
+if __name__ == '__main__':
+  X = 'ABCBDAB'
+  Y = 'BDCABA'
+  c, b = lcs_length(X, Y)
+  print_lcs(b, X, len(X), len(Y))
+
